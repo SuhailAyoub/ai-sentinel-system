b16a0479081a69dcbbd16a717dffc1c948fe2eeb
Suhail <suhailayoub797@gmail.com>
2026-05-17 20:51:38 +0530

    Fixed render port

--- app.py
+++ app.py
@@ -366,5 +366,9 @@ def google_login():
     return redirect('/dashboard')
 
 # ---------------- RUN APP ----------------
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
